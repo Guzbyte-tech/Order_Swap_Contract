3c8fe142abad2029da814be2ac2e0d4c01ae83d4
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2024-09-19 14:45:05 +0100

    Added Cancel Order

--- contracts/OrderSwap.sol
+++ contracts/OrderSwap.sol
@@ -11,7 +11,8 @@ contract OrderSwap {
 
     enum OrderStatus {
         Open,
-        Closed
+        Closed,
+        Cancelled
     }
 
     uint public totalOrders;
@@ -44,25 +45,23 @@ contract OrderSwap {
         require(_depositedAmount > 0, "Invalid Deposit Amount");
         require(_requestedAmount > 0, "Invalid Requested Amount");
 
-        IERC20(_depositorToken).transferFrom(
-            msg.sender,
-            address(this),
-            _depositedAmount
+        require(
+            IERC20(_depositorToken).transferFrom(
+                msg.sender,
+                address(this),
+                _depositedAmount
+            ),
+            "Transfer Failed"
         );
 
-        uint orderId = totalOrders + 1;
-        Order storage ord = OrderIdToOrders[orderId];
-        ord.orderId = orderId;
-        ord.depositor = msg.sender;
-        ord.depositorToken = _depositorToken;
-        ord.depositedAmount = _depositedAmount;
-        ord.requestedToken = _requestedToken;
-        ord.requestAmount = _requestedAmount;
-        ord._orderType = OrderType.OrderCreated;
-        ord.status = OrderStatus.Open;
-
-        totalOrders = orderId;
-
+        createTransaction(
+            _depositedAmount,
+            _depositorToken,
+            _requestedAmount,
+            _requestedToken,
+            OrderType.OrderCreated,
+            OrderStatus.Open
+        );
         //Emit an Event OrderCreatedSuccessfully.
     }
 
@@ -71,6 +70,10 @@ contract OrderSwap {
         require(
             OrderIdToOrders[_orderId].status == OrderStatus.Open,
             "Order Already Fulfiled"
+        );
+        require(
+            OrderIdToOrders[_orderId].status == OrderStatus.Cancelled,
+            "Order Cancelled"
         );
 
         //send requested token to depositor.
@@ -83,11 +86,63 @@ contract OrderSwap {
             "Transfer requested token failed."
         );
 
-        require(IERC20(OrderIdToOrders[_orderId].depositorToken).transfer(
-            msg.sender,
-            OrderIdToOrders[_orderId].depositedAmount
-        ), "Transfer to fulfiller failed");
+        require(
+            IERC20(OrderIdToOrders[_orderId].depositorToken).transfer(
+                msg.sender,
+                OrderIdToOrders[_orderId].depositedAmount
+            ),
+            "Transfer to fulfiller failed"
+        );
 
-        
+        OrderIdToOrders[_orderId].fulfilledBy = msg.sender;
+        OrderIdToOrders[_orderId].status = OrderStatus.Closed;
+        //Create Order History For The Fulfiller.
+        createTransaction(
+            OrderIdToOrders[_orderId].depositedAmount,
+            OrderIdToOrders[_orderId].depositorToken,
+            OrderIdToOrders[_orderId].requestAmount,
+            OrderIdToOrders[_orderId].requestedToken,
+            OrderType.OrderFulfiled,
+            OrderStatus.Closed
+        );
+    }
+
+    function cancelOrder(uint _orderId) external {
+        require(OrderIdToOrders[_orderId].orderId > 0, "Invalid Order Id");
+        require(
+            OrderIdToOrders[_orderId].status == OrderStatus.Open,
+            "Order Already Fulfiled"
+        );
+        require(
+            OrderIdToOrders[_orderId].status == OrderStatus.Cancelled,
+            "Order Cancelled"
+        );
+        //Check if depositor is the msg.sender
+        require(
+            OrderIdToOrders[_orderId].depositor == msg.sender,
+            "Not Order Owner."
+        );
+        OrderIdToOrders[_orderId].status = OrderStatus.Cancelled;
+    }
+
+    function createTransaction(
+        uint _depositedAmount,
+        address _depositorToken,
+        uint _requestedAmount,
+        address _requestedToken,
+        OrderType _type,
+        OrderStatus _status
+    ) private {
+        uint orderId = totalOrders + 1;
+        Order storage ord = OrderIdToOrders[orderId];
+        ord.orderId = orderId;
+        ord.depositor = msg.sender;
+        ord.depositorToken = _depositorToken;
+        ord.depositedAmount = _depositedAmount;
+        ord.requestedToken = _requestedToken;
+        ord.requestAmount = _requestedAmount;
+        ord._orderType = _type;
+        ord.status = _status;
+        totalOrders = orderId;
     }
 }
